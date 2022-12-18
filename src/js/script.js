0c2e65d6f72be8240e21ebe6f439a6ca55ebb087
solution <dev.ephemeral666@gmail.com>
2022-12-19 01:54:24 +0200

    ended 23 lesson

--- src/js/script.js
+++ src/js/script.js
@@ -194,7 +194,255 @@ console.log(2+2*2 !== '6');
  */
 
 
-/* lesson 16 */
+/* lesson 20 Условия */
 
 
+/* if (4 == 4) {
+    console.log('OK!');
+}
 
+if (4 == 9) {
+    console.log('OK!');
+} else {
+    console.log('Error');
+}
+
+if (1) {
+    console.log('OK!');
+}
+
+const num = 50;
+
+if(num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Over');
+} else {
+    console.log('OK');
+}
+
+(num === 50) ? console.log('OK') : console.log('Error');  Условный (тернарный) оператор - 
+единственный оператор в JavaScript, принимающий три операнда: условие,
+ за которым следует знак вопроса (?),затем выражение,
+ которое выполняется, если условие истинно, сопровождается двоеточием
+ (:), и, наконец, выражение, которое выполняется, если условие ложно.
+Он часто используется в качестве укороченного варианта условного оператора if. */
+
+/* const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('No');
+        break;
+    case 100:
+        console.log('No');
+        break;
+    case 50:
+        console.log('Yes');
+        break;
+    default:
+        console.log('No now');
+        break;
+} */
+
+
+/* lesson 21 (д) Логические операторы */
+
+
+/* && */
+
+/* const hamburger = true;
+const fries = true; (false)
+
+if (hamburger && fries) {
+    console.log(`I'm full`);
+}
+
+console.log((hamburger && fries)); */
+
+/* const hamburger = 5;
+const fries = 0; (null; undefined)
+
+if (hamburger && fries) {
+    console.log(`I'm full`);
+}
+
+console.log((hamburger && fries)); */
+
+/* const hamburger = 3;
+const fries = 1;
+const cola = 1;
+
+false - 0, NaN, undefined, null, '';
+
+console.log(hamburger === 3 && cola && fries);
+
+console.log(1 && 0);
+console.log(1 && 5);
+console.log(null && 5);
+console.log(0 && 'hell');
+console.log(1 && 'hell');
+
+if (hamburger === 3 && cola === 1 && fries) {
+    console.log(`All fulls`);
+} else {
+    console.log("We're leaves");
+} */
+
+/* || */
+
+/* const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger || cola || fries) {
+    console.log(`All happys`);
+} else {
+    console.log("We're leaves");
+}
+
+if (hamburger === 3 && cola == 2 || fries === 3 && nuggets) {
+    console.log(`All happys`);
+} else {
+    console.log("We're leaves");
+}
+
+console.log(hamburger === 3 && cola == 2 || fries === 3 && nuggets);
+console.log(hamburger === 3 && (cola == 2 || fries === 3) && nuggets);
+console.log(hamburger || cola || fries);
+
+let johnRport, alexReport, samReport, mariaReport = 'done';
+
+console.log(johnRport || alexReport || samReport || mariaReport); */
+
+/* ! */
+
+/* console.log(!0); false(это не false) > true(это true) */
+
+
+/* HOMEWORK */
+
+
+/* console.log( NaN || 2 || undefined );   2 
+ 
+console.log( NaN && 2 && undefined );   NaN
+ 
+console.log( 1 && 2 && 3 );   3
+ 
+console.log( !1 && 2 || !3 );   false
+ 
+console.log( 25 || null && !3 );   25
+ 
+console.log( NaN || null || !3 || undefined || 5);   5
+ 
+console.log( NaN || null && !3 && undefined || 5);   5
+ 
+console.log( 5 === 5 && 3 > 1 || 5);   true */
+
+/* const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger === 3 && cola || fries === 3 && nuggets) {
+   console.log('Done!');
+} true */
+
+/* let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger || cola || fries === 3 || nuggets) {
+   console.log('Done!');
+} true */
+
+/* let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!');
+} false */
+
+
+/* lesson 22 Циклы */
+
+
+/* let num = 50;
+ while (num <= 55) {
+    console.log(num);
+    num++;
+ }
+
+ do {
+    console.log(num);
+    num++;
+ }
+ while (num <= 55);
+
+ for (let i = 1; i < 8; i++) {
+    console.log(num);
+    num++;
+ }
+
+ for (let i = 1; i < 8; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+
+    console.log(i); 
+ } */
+
+
+ /* lesson 23 Циклы в цикле и метки */
+
+
+/* for (let i = 0; i <3;  i++) {
+    console.log(i);
+    for (let j = 0; j <3;  j++) {
+        console.log(j);
+    }
+} */
+
+/* let result = '';
+const len = 7;
+
+for (let i = 1; i < len; i++) {
+
+    for (let j = 0; j < i; j++) {
+        result += "*";
+    }
+
+    result += '\n';
+}
+
+console.log(result); */
+
+/* first: for (let i = 0; i <3;  i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j <3;  j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k <3;  k++) {
+            if (k === 2) break first;
+            console.log(`Third level: ${k}`);
+        }
+    }
+} */
+
+const lines = 5;
+let result = '';
+
+for (let i = 1; i < lines; i++) {
+    
+    for (let j = 0; j < i; j++) {
+        result += '*';
+    }
+    
+    result += '\n';
+}
+
+console.log(result);
