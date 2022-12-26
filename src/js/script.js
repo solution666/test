f07d1f985d08eeacb4665feabd4b59f58bdd8260
solution <dev.ephemeral666@gmail.com>
2022-12-26 17:21:58 +0200

    Homework_cycle_end!

--- src/js/script.js
+++ src/js/script.js
@@ -484,16 +484,25 @@ for (let i = 0; i < data.length; i++) {
 
 console.log(result); */
 
-const lines = 5;
+/* const lines = 5;
 let result = '';
 
-for (let i = 1; i < lines; i++) {
-    
-    for (let j = 0; j < i; j++) {
-        result += '*';
+for (let i = 0; i <= lines; i++) {
+        
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
     }
-    
+
+    for (let j = 0; j < i * 2 + 1; j++) {
+        result += "*";
+    }
+
     result += '\n';
 }
 
-console.log(result);
+console.log(result); */
+
+
+/* lesson 22 Функции, стрелочные ф-ции (ES6) */
+
+
