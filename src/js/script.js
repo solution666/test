f7b2a9d7f25f373c854680e6ac82c3c2164e2884
solution <dev.ephemeral666@gmail.com>
2023-01-07 21:13:59 +0200

    hoMEwork/Obj

--- src/js/script.js
+++ src/js/script.js
@@ -1123,4 +1123,72 @@ console.log(newObj); */
                     /* lesson 38 Основы ООП, прототипно-ориентированное наследование */
 
 
-let me = 'down' + ' ' + 'slowly' + '!';
+/* let str = "some";
+let strObj = new String(str);
+
+console.log(typeof(str));
+console.log(typeof(strObj));
+
+console.dir([1, 2, 3]);
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log('Hello');
+    }
+};
+
+це:
+const john = Object.create(soldier);
+
+рівне цьому:
+const john = {
+    health: 100
+};
+
+john.__proto__ = soldier;
+
+Object.setPrototypeOf(john, soldier);
+
+console.log(john.armor);
+john.sayHello(); */
+
+
+                    /* HOMEWORK */
+
+
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
+};
+
+function isBudgetEnough(data) {
+
+}
+
+console.log(isBudgetEnough(shoppingMallData));
+
+
+                    /* lesson 39 Динамическая типизация в JS */
+
+
