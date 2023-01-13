1ebbb01ff7e1e2a828ecbc0bff08ccce992edb40
solution <dev.ephemeral666@gmail.com>
2023-01-13 17:49:03 +0200

    before training

--- src/js/script.js
+++ src/js/script.js
@@ -1415,3 +1415,91 @@ function transferWaitors(data) {
 }
 
 transferWaitors(restorantData); */
+
+
+                    /* lesson 42 Получение элементов со страницы */
+
+
+/* const box = document.getElementById('box');
+
+console.log(box);
+
+const btns = document.getElementsByTagName('button');
+const btns = document.getElementsByTagName('button')[1];
+
+console.log(btns[1]);
+
+const circles = document.getElementsByClassName('circle');
+
+console.log(circles);
+
+const hearts = document.querySelectorAll('.heart');
+
+console.log(hearts);
+
+hearts.forEach(item => {
+    console.log(item);
+});
+
+const oneHeart = document.querySelector('#box');
+
+console.log(oneHeart); */
+
+
+                    /* lesson 42 Получение элементов со страницы */
+
+
+let num = 500;
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      wrapper = document.querySelector('.wrapper'),
+      hearts = wrapper.querySelectorAll('.heart'),
+      oneHeart = wrapper.querySelector('.heart');
+
+box.style.beckgroundColor = 'blue';
+box.style.width = '500px';
+
+box.style.cssText = 'background-color: blue; width: 500px';
+box.style.cssText = `background-color: blue; width: ${num}px`;
+
+btns[1].style.borderRadius = '100%';
+circles[0].style.beckgroundColor = 'red';
+
+for (let i = 0; i < hearts.length; i++) {
+    hearts[i].style.beckgroundColor = 'blue';
+}
+
+hearts.forEach(item => {
+    item.style.beckgroundColor = 'blue';
+});
+
+const div = document.createElement('div');
+const text = document.createTextNode('I am here!');
+
+div.classList.add('black');
+
+document.body.append(div);
+wrapper.append(div);
+wrapper.appendChild(div);
+
+wrapper.prepend(div);
+
+hearts[0].befor(div);
+hearts[0].after(div);
+
+wrapper.insertBefore(div, hearts[1]);
+
+circles[0].remove();
+wrapper.removeChild(hearts[1]);
+
+hearts[0].replaceWith(circles[0]);
+wrapper.replaceChild(circles[0], hearts[0]);
+
+div.innerHTML = "<h1>Hello, World!</h1>";
+div.textContent = 'Hello';
+
+div.insertAdjacentHTML('afterbegin', "<h2>Hello1</h2>");
+div.insertAdjacentHTML('afterend', "<h2>Hello2</h2>");
+div.insertAdjacentHTML('beforebegin', "<h2>Hello3</h2>");
+div.insertAdjacentHTML('beforeend', "<h2>Hello4</h2>");
